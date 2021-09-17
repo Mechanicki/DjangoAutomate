4247e02f51a9e8986d36f1fa861750c3ea6898aa
Adam <adam.stepien97@gmail.com>
2021-09-17 09:52:55 +0200

    settings Change Engine to env value

--- website/website/settings.py
+++ website/website/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-       'ENGINE': 'django.db.backends.postgresql',
+       'ENGINE': config('ENGINE'),
        'NAME': config('POSTGRES_DB'),
        'USER': config('POSTGRES_USER'),
        'PASSWORD': config('POSTGRES_PASSWORD'),
